921b43afc8a3c9c5603bf212d1071cea36c6cd47
Moritz Brödel <moritz.broedel@gmail.com>
2025-05-16 15:27:11 +0200

    Fix stack allocation

--- src/main/java/edu/kit/kastel/vads/compiler/backend/RegisterAllocator.kt
+++ src/main/java/edu/kit/kastel/vads/compiler/backend/RegisterAllocator.kt
@@ -14,7 +14,7 @@ class RegisterAllocator {
         val stackSize = instructions
             .flatMap { it.operands.toList() }
             .filterIsInstance<SpilledRegister>()
-            .maxOfOrNull { - it.offset + 4 } ?: 0
+            .maxOfOrNull { -it.offset + 4 } ?: 0
         val prolog = listOf(
             Instruction(Name.PUSHQ, PointerRegisters.RBP),
             Instruction(Name.MOVQ, PointerRegisters.RSP, PointerRegisters.RBP),
@@ -34,22 +34,25 @@ class RegisterAllocator {
 
     private fun produceRegisters(): Iterator<RealRegister> = sequence {
         GeneralRegisters.entries
-            .filter { it !in listOf(GeneralRegisters.EAX, GeneralRegisters.EDX) }
+            .filter { it !in listOf(GeneralRegisters.EAX, GeneralRegisters.EDX, TEMP_REG) }
             .forEach { yield(it) }
-        generateSequence(0) { it - 4 }
+        generateSequence(-4) { it - 4 }
             .forEach { yield(SpilledRegister(it)) }
     }.iterator()
 
     private fun eliminateMemToMemInstructions(instructions: List<Instruction>): List<Instruction> {
-        return instructions.flatMap { inst -> when {
-            inst.operands.filterIsInstance<SpilledRegister>().size == 2 -> {
-                val (src, dst) = inst.operands
-                listOf(
-                    Instruction(Name.MOVL, src, TEMP_REG),
-                    Instruction(inst.name, TEMP_REG, dst),
-                )
+        return instructions.flatMap { inst ->
+            when {
+                inst.operands.filterIsInstance<SpilledRegister>().size == 2 -> {
+                    val (src, dst) = inst.operands
+                    listOf(
+                        Instruction(Name.MOVL, src, TEMP_REG),
+                        Instruction(inst.name, TEMP_REG, dst),
+                    )
+                }
+
+                else -> listOf(inst)
             }
-            else -> listOf(inst)
-        } }
+        }
     }
 }
